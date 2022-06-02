81345e3e17c0308dec047e3c6d388e62cccc22d7
theo-slick <theo.levisage@slickteam.fr>
2022-06-02 16:11:20 +0200

    getters and setters

--- src/main/java/fr/slickteam/entity/Dog.java
+++ src/main/java/fr/slickteam/entity/Dog.java
@@ -15,4 +15,36 @@ public class Dog {
     @ManyToOne(targetEntity=Human.class, fetch=FetchType.EAGER)
     @JoinColumn(name="human_id")
     public Human owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    public Human getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Human owner) {
+        this.owner = owner;
+    }
 }
